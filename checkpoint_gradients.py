8c99209a97f81e4bcc06da09c3996320b91d7fa2
Sirius083 <1518246621@qq.com>
2019-04-15 11:21:49 +0800

    Update checkpoint_gradients.py

--- checkpoint_gradients.py
+++ checkpoint_gradients.py
@@ -1,37 +1,39 @@
-    https://github.com/joeyearsley/efficient_densenet_tensorflow
-    def _conv_block(self, ip, nb_filter):
-        """ Apply BatchNorm, Relu, 3x3 Conv2D, optional bottleneck block and dropout
+# https://github.com/joeyearsley/efficient_densenet_tensorflow
+# memory efficient implementation of densenet
 
-            Args:
-                ip: Input tensor
-                nb_filter: number of filters
+def _conv_block(self, ip, nb_filter):
+    """ Apply BatchNorm, Relu, 3x3 Conv2D, optional bottleneck block and dropout
 
-            Returns: tensor with batch_norm, relu and convolution2d added (optional bottleneck)
-        """
+        Args:
+            ip: Input tensor
+            nb_filter: number of filters
 
-        def _x(ip):
-            x = batch_normalization(ip, **self.bn_kwargs)
+        Returns: tensor with batch_norm, relu and convolution2d added (optional bottleneck)
+    """
+
+    def _x(ip):
+        x = batch_normalization(ip, **self.bn_kwargs)
+        x = tf.nn.relu(x)
+
+        if self.bottleneck:
+            inter_channel = nb_filter * 4
+
+            x = conv2d(x, inter_channel, (1, 1), kernel_initializer='he_normal', padding='same', use_bias=False,
+                       **self.conv_kwargs)
+            x = batch_normalization(x, **self.bn_kwargs)
             x = tf.nn.relu(x)
 
-            if self.bottleneck:
-                inter_channel = nb_filter * 4
+        x = conv2d(x, nb_filter, (3, 3), kernel_initializer='he_normal', padding='same', use_bias=False,
+                   **self.conv_kwargs)
 
-                x = conv2d(x, inter_channel, (1, 1), kernel_initializer='he_normal', padding='same', use_bias=False,
-                           **self.conv_kwargs)
-                x = batch_normalization(x, **self.bn_kwargs)
-                x = tf.nn.relu(x)
+        if self.dropout_rate:
+            x = dropout(x, self.dropout_rate, training=self.training)
 
-            x = conv2d(x, nb_filter, (3, 3), kernel_initializer='he_normal', padding='same', use_bias=False,
-                       **self.conv_kwargs)
+        return x
 
-            if self.dropout_rate:
-                x = dropout(x, self.dropout_rate, training=self.training)
+    if self.efficient:
+        # efficient implementation: recompute gradient on the backward pass
+        # Gradient checkpoint the layer
+        _x = tf.contrib.layers.recompute_grad(_x)
 
-            return x
-
-        if self.efficient:
-            # efficient implementation: recompute gradient on the backward pass
-            # Gradient checkpoint the layer
-            _x = tf.contrib.layers.recompute_grad(_x)
-
-        return _x(ip)
+    return _x(ip)
